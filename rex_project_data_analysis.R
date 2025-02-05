402b611ab11690f8a066db88d0eb5b46105c315c
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-02-05 10:42:33 -0800

    update notation

--- rex_project_data_analysis.R
+++ rex_project_data_analysis.R
@@ -1,8 +1,8 @@
 library("tidyverse")
 
 # uploading data
-mpox_cases_raw_unfiltered <- read_csv("afr_cases_as_of_february_2nd.csv")
-temperature_burundi_unfiltered <- read_csv("Burundi_temperature.csv")
+mpox_cases_raw_unfiltered <- read_csv('afr_cases_as_of_february_2nd.csv')
+temperature_burundi_unfiltered <- read_csv('Burundi_temperature.csv')
 
 # filtering data
 burundi_only <- mpox_cases_raw_unfiltered %>%
