08f525fbc59b4083b70691822994e098633ed7dc
Rachel <124736890+JinChun5239@users.noreply.github.com>
2025-02-11 14:52:10 -0800

    merge meteorological and mpox data

--- rex_project_data_analysis.R
+++ rex_project_data_analysis.R
@@ -3,27 +3,27 @@ library(dplyr)
 
 # uploading data
 mpox_cases_raw_unfiltered <- read_csv('afr_cases_as_of_february_2nd.csv')
-temperature_burundi_unfiltered <- read_csv('Burundi_temperature.csv')
+temperature_burundi_unfiltered <- read_csv('correct_metereological_data.csv')
 
 # filtering data
-burundi_only <- mpox_cases_raw_unfiltered %>%
-    filter(country == "Burundi") %>%
-    arrange(week_end_date)
-
-# computing temperature data so that it's weekly
-# index by week
-temperature_burundi_unfiltered$seven_day_index <- c(0, rep(1:(nrow(temperature_burundi_unfiltered)-1)%/%7))
-# remove irrelevant columns
-meteor_bujumbura <- subset(temperature_burundi_unfiltered, 
-                           select = -c(name, datetime, 
-                                       preciptype:snowdepth, 
-                                       severerisk:stations)) 
-# remove last row as it only contains one day for that week
-meteor_bujumbura <- meteor_bujumbura %>% filter(row_number() <= n()-1)
-print("seven day index: 0 for 2024-07-21 to 2024-07-27 inclusive, 
-      ending at 2025-01-25 to 2025-02-01 index/week 27")
-meteor_bujumbura <- meteor_bujumbura %>%
-  group_by(seven_day_index) %>%
-  summarise(across(c(tempmax:uvindex), mean),
-            .groups = 'drop') %>%
+burundi_cases <- mpox_cases_raw_unfiltered %>%
+  filter(country == "Burundi") %>%
+  subset(select = c(week_end_date:total_suspected_cases, 
+                    new_confirmed_cases:new_suspected_cases)) %>%
+  arrange(week_end_date)
+burundi_cases$ID <- c(0: (nrow(burundi_cases) - 1))
+temperature_burundi_unfiltered$ID <- c(0, rep(1:(nrow(temperature_burundi_unfiltered)-1)%/%7))
+meteor_bujumbura <- temperature_burundi_unfiltered %>%
+  subset(select = -c(name, datetime, feelslikemax:feelslike, 
+                     preciptype:snowdepth, severerisk:stations)) %>%
+  group_by(ID) %>%
+  summarise(across(c(tempmax:uvindex), mean), .groups = 'drop') %>%
   as.data.frame()
+
+# match variables to existing papers
+meteor_bujumbura <- subset(meteor_bujumbura, 
+                           select = c(ID, temp, dew:precip, windspeed))
+
+# merge datasets
+inner_join <- merge(meteor_bujumbura, burundi_cases, by = "ID")
+
